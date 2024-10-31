58bc06ab85fbe947ed1f3e0fb7a846b21a08cafc
olgalvova2024 <olga.lvova1317@mail.ru>
2024-10-31 20:57:22 +0300

    Задача "Продуктовая база"

---
+++ crud_functions.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+def initiate_db():
+    connection = sqlite3.connect('database_14_4.db')
+    cursor = connection.cursor()
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products(
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    price INT NOT NULL
+    )
+    ''')
+    cursor.execute('CREATE INDEX IF NOT EXISTS idx_id ON Products(id)')
+
+    for i in range(1,5):
+        cursor.execute('INSERT INTO Products(title, description, price) VALUES(?,?,?)',
+        (f'Продукт{i}', f'Описание{i}', f'{i*100}'))
+
+    connection.commit()
+    connection.close()
+
+def get_all_products():
+    connection = sqlite3.connect('database_14_4.db')
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM Products')
+    all_products = cursor.fetchall()
+    connection.commit()
+    connection.close()
+    return all_products
